3ccbc2228f0079830a6739a1a2ba8d4a28f3a179
Adam Ford <adam.ford@greatbigsolutions.com>
2015-01-31 13:29:47 -0700

    Add ExpenseItem

---
+++ src/cmput301/ajford_notes/models/ExpenseItem.java
@@ -0,0 +1,36 @@
+package cmput301.ajford_notes.models;
+
+import java.util.Date;
+import org.joda.money.Money;
+
+public class ExpenseItem {
+	private Date date;
+	private String category;
+	private String description;
+	private Money amountSpent;
+
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Money getAmountSpent() {
+		return amountSpent;
+	}
+	public void setAmountSpent(Money amountSpent) {
+		this.amountSpent = amountSpent;
+	}
+}
