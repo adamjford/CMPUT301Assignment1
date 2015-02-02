8a69b924fc85ae8e0114341ae5a1b795ee50dd3a
Adam Ford <adam.ford@greatbigsolutions.com>
2015-02-02 10:27:19 -0700

    Add valid categories

--- src/cmput301/ajford/expense_tracker/models/ExpenseItem.java
+++ src/cmput301/ajford/expense_tracker/models/ExpenseItem.java
@@ -1,9 +1,25 @@
 package cmput301.ajford.expense_tracker.models;
 
+import java.util.ArrayList;
 import java.util.Date;
 import org.joda.money.Money;
 
 public class ExpenseItem {
+	private transient static final ArrayList<String> validCategories = new ArrayList<String>() {{
+		add("Air Fare");
+		add("Ground Transport");
+		add("Vehicle Rental");
+		add("Fuel");
+		add("Parking");
+		add("Registration");
+		add("Accomodiation");
+		add("Meal");
+	}};
+	
+	public static ArrayList<String> getValidCategories() {
+		return validCategories;
+	}
+	
 	private Date date;
 	private String category;
 	private String description;
@@ -12,24 +28,31 @@ public class ExpenseItem {
 	public Date getDate() {
 		return date;
 	}
+
 	public void setDate(Date date) {
 		this.date = date;
 	}
+
 	public String getCategory() {
 		return category;
 	}
+
 	public void setCategory(String category) {
 		this.category = category;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public Money getAmountSpent() {
 		return amountSpent;
 	}
+
 	public void setAmountSpent(Money amountSpent) {
 		this.amountSpent = amountSpent;
 	}
